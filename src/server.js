ae2675b2bedde5a25e6c83397c44c1e052cb9705
shohedul350 <shohedul350@gmail.com>
2020-10-15 22:38:41 +0600

    refactoring

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ import app from './app';
 import connectDB from './mongo';
 
 const PORT = process.env.PORT || 5000;
-connectDB();
+
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`);
 });
